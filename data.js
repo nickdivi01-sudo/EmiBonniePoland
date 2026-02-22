e14a7e1b8916528cb22b831aee6ad09162d0cc9e
nickdivi01-sudo <nick.divi.01@gmail.com>
2026-02-22 20:47:48 +0100

    Create data.js file for storing editable actress data

---
+++ data.js
@@ -0,0 +1,13 @@
+// Editable actress data
+
+const actresses = [
+    {
+        name: "Actress Name",
+        age: 30,
+        movies: ["Movie Title 1", "Movie Title 2"],
+        bio: "Short bio here."
+    },
+    // Add more actresses as needed
+];
+
+export default actresses;
